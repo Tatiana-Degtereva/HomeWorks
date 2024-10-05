ed65c0bea91a38b5c2031d946f3b6417d9af941a
Tatiana-Degtereva <ryzhaja.sterva@gmail.com>
2024-10-06 02:12:00 +0700

    2024/10/06

--- peremennye.py
+++ peremennye.py
@@ -1,5 +1,5 @@
 completed_homework = 12
 hours_spent = 1.5
-Course_name = 'Python'
-Time_for_one_task = hours_spent/completed_homework
-print('Курс:', Course_name, ', всего задач:', completed_homework, ', затрачено часов:', hours_spent, ', среднее время выполнения ', Time_for_one_task,' часа.')
+course_name = 'Python'
+time_for_one_task = hours_spent/completed_homework
+print('Курс:', course_name, ', всего задач:', completed_homework, ', затрачено часов:', hours_spent, ', среднее время выполнения ', time_for_one_task,' часа.')
