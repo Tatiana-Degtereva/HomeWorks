6975329feaa9cce8b1a2fcde2ff6c45ad051d560
Tatiana <ryzhaja.sterva@gmail.com>
2024-12-08 23:19:02 +0700

    08/12

---
+++ module_5_1.py
@@ -0,0 +1,28 @@
+# Домашняя работа по уроку "Атрибуты и методы объекта"
+# Задача "Developer - не только разработчик":
+# Реализуйте класс House, объекты которого будут создаваться следующим образом:
+# House('ЖК Эльбрус', 30)
+# Объект этого класса должен обладать следующими атрибутами:
+# self.name - имя, self.number_of_floors - кол-во этажей
+# Также должен обладать методом go_to(new_floor), где new_floor - номер этажа(int), на который нужно приехать.
+# Метод go_to выводит на экран(в консоль) значения от 1 до new_floor(включительно).
+# Если же new_floor больше чем self.number_of_floors или меньше 1, то вывести строку "Такого этажа не существует".
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        self.new_floor = int(new_floor)
+        if self.new_floor > self.number_of_floors or self.new_floor < 1:
+            print('такого этажа не существует')
+        else:
+            for i in range(1, self.new_floor):
+                print(i)
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
